2fc2e88e4901adb1528e2c852e97ac8893444233
Jussi Tiira <jussi@j24.fi>
2021-10-26 20:54:36 +0300

    herb figure slideshow

--- translations/harbour-yrttipiha-fi.ts
+++ translations/harbour-yrttipiha-fi.ts
@@ -45,7 +45,7 @@
 <context>
     <name>Herb</name>
     <message>
-        <location filename="../qml/pages/Herb.qml" line="63"/>
+        <location filename="../qml/pages/Herb.qml" line="74"/>
         <source> family (</source>
         <translation>-heimo (</translation>
     </message>
